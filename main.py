9a8416c130117fc49383e52750b029d103d9b3b4
париж <x.arkhangel@yandex.ru>
2026-03-30 23:07:58 +0300

    редукция. мягкий и твердый знаки

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-primer = "молокО"
-nothingvow = 'аоуэ'
-softvow = 'и'
-jvow = 'еёюя'
-vow = 'аоуэиеёюя'
+primer = "глУхонький"
+nothingvow = "аоуэ"
+softvow = "и"
+jvow = "еёюя"
+vow = "аоуэиеёюя"
 # звонкий + глухой = звонкий
-dingcon = ['б', 'в', 'г', 'д', 'ж', 'з']
-shhcon = ['п', 'ф', 'к', 'т', 'ш', 'с']
-reducts = {'а': ['а', 'а', 'ь'], 'о': ['о', 'а', 'ъ'], 'у': ['у', 'у', 'ъ'], 'э': ['э', 'э', 'ъ'], 'и': ['и', 'и', 'ь'],
-           'е': ["`э", '`и', '`ь'], 'ё': ['`о', '`о', '`о'], 'ю': ['`у', '`у', '`ъ'], 'я': ['`а', '`а', '`ь']}
+dingcon = ["б", "в", "г", "д", "ж", "з"]
+shhcon = ["п", "ф", "к", "т", "ш", "с"]
+reducts = {"а": ["а", "а", "ь"], "о": ["о", "а", "ъ"], "у": ["у", "у", "ъ"], "э": ["э", "э", "ъ"], "и": ["и", "и", "ь"],
+           "е": ["'э", "'и", "'ь"], "ё": ["'о", "'о", "'о"], "ю": ["'у", "'у", "'ъ"], "я": ["'а", "'а", "'ь"]}
 
 
 def reduction(word):
@@ -17,13 +17,13 @@ def reduction(word):
         c = word[i]
         low = c.lower()
         if low in vow:
-            if ord(c) < ord('а'):
+            if ord(c) < ord("а"):
                 if reds:
                     reds[-1] = (reducts[prev[0]][1], prev[1])
                 reds.append((reducts[low][0], i))
                 udar = True
             elif udar:
-                reds.append((reducts[low][1], i))
+                reds.append((reducts[low][2], i))
                 udar = False
             else:
                 reds.append((reducts[low][2], i))
@@ -34,10 +34,25 @@ def reduction(word):
     for i in reds:
         result[i[1]] = i[0]
         print(i, result)
-    ans = ''
+    ans = ""
     for i in result:
         ans += i
     return ans, reds
+
+
+def softness(word):
+    ans = [word[0]]
+    prev = word[0]
+    for i in range(1, len(word)):
+        if word[i] == "ь":
+            ans.append("'")
+            ans.append(word[i])
+        elif word[i] == "ъ" and prev == "'":
+            ans.pop()
+    ret = ''
+    for c in ans:
+        ret += c
+    return ret
 
 
 print(reduction(primer))
@@ -46,4 +61,4 @@ print(reduction(primer))
 def transcr(word: str, stress: int):
     n = len(word)
     phoword = word
-    return f'[{phoword}]'
+    return f"[{phoword}]"
